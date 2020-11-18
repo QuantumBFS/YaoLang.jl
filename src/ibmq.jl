4b4637fdcfbbecf154d7b7b7cbfd5ef3480eef4e
Roger-luo <rogerluo.rl18@gmail.com>
2020-11-17 23:21:43 -0500

    implement basic interface

--- src/ibmq.jl
+++ src/ibmq.jl
@@ -4,21 +4,64 @@ using YaoCompiler
 using IBMQClient
 using YaoAPI
 using REPL.TerminalMenus
+using Crayons.Box
+using IBMQClient: AccountInfo, IBMQDevice, DeviceMenu
 
-mutable struct AccountInfo
-    access_token::String
-    auth::IBMQClient.AuthAPI
-    service::IBMQClient.ServiceAPI
-    project::IBMQClient.ProjectAPI
-end
+# RL: qiskit stores it on disk anyway, so I think it's fine for us to
+# store it temporarily in a Julia instance just for convenience
 
+"account cache to speed up register creation and queries"
 const account_cache = Dict{String, AccountInfo}()
 
+"""
+    IBMQReg <: AbstractRegister{1}
+
+A virtual IBM Q register for uploading jobs to IBM Q REST API.
+"""
 struct IBMQReg <: AbstractRegister{1}
-    device::IBMQClient.IBMQDevice    
+    account::AccountInfo
+    device::IBMQDevice
+
+    nshots::Int
+    memory_slots::Int
+    nqubits::Int
+
+    options::NamedTuple
+    # max_credits::Int
+    # seed::Int 
+    # schema_version::String
+    # type::String
+    # memory::Bool
+    # init_qubits::Bool
+    # parameter_binds::Vector
+    # parametric_pulses
 end
 
-function IBMQReg(;token::Union{Nothing, String}=nothing)
+function Base.show(io::IO, m::MIME"text/plain", r::IBMQReg)
+    println(io, "IBM Q (virtual) register:")
+    show(IOContext(io, :indent=>2), m, r.account)
+    println(io)
+    println(io)
+    show(IOContext(io, :indent=>2), m, r.device)
+end
+
+function IBMQReg(;
+        token::Union{Nothing, String}=nothing,
+        device::Union{Nothing, String}=nothing,
+        nshots::Int = 1024,
+        memory_slots::Int=-1,
+        nqubits::Int=-1, # NOTE: we use nqubits here for consistency
+        kw...
+        # max_credits::Int = 3,
+        # seed::Int = 1,
+        # schema_version::String="1.3.0",
+        # type::String="QASM",
+        # memory::Bool=false,
+        # init_qubits::Bool=true,
+        # parameter_binds::Vector=[],
+        # parametric_pulses=[],
+    )
+
     if token === nothing
         if haskey(ENV, "IBMQ_TOKEN")
             token = ENV["IBMQ_TOKEN"]
@@ -29,43 +72,94 @@ function IBMQReg(;token::Union{Nothing, String}=nothing)
         end
     end
 
-    auth = IBMQClient.AuthAPI()
-    response = IBMQClient.login(auth, token)
-    access_token = response["id"]
-    user_info = IBMQClient.user_info(auth, access_token)
-    service = IBMQClient.ServiceAPI(user_info["urls"]["http"])
-    user_hub = first(IBMQClient.user_hubs(service, access_token))
-    project = IBMQClient.ProjectAPI(service.endpoint, user_hub.hub, user_hub.group, user_hub.project)
+    global account_cache
+    if haskey(account_cache, token)
+        account = account_cache[token]
+    else
+        account = AccountInfo(token)
+        account_cache[token] = account
+    end
 
-    global  account_cache
-    account_cache[access_token] = AccountInfo(access_token, auth, service, project)
+    devices = IBMQClient.devices(account)
 
-    devices = IBMQClient.devices(project, access_token)
-    menu = RadioMenu(map(x->x.name, devices), pagesize=4)
-    choice = request("choose a device:", menu)
-    return IBMQReg(devices[choice])
+    if device === nothing
+        menu = DeviceMenu(devices, pagesize=6)
+        choice = request("choose a device:", menu)
+        if choice < 0
+            return
+        end
+    else
+        dev = findfirst(x->x.name == device, devices)
+        dev === nothing || error("device \"$device\" is not available.")
+    end
+    return IBMQReg(
+        account, devices[choice], nshots,
+        memory_slots, nqubits, kw,
+    )
 end
 
-# macro ibmq(ex...)
-# end
+function create_main_qobj(r::IBMQReg, specs::RoutineSpec...)
+    target = YaoCompiler.TargetQobjQASM(r.nshots, r.seed, r.max_credits)
 
-# function ibmq_m(ex)
-#     quote
-#         spec = $ex
-#         ci, = @code_yao optimize=true spec
-#         target = YaoCompiler.TargetQobjQASM()
-#         qobj = YaoCompiler.codegen(target, ci)
+    experiments = map(specs) do spec
+        # TODO: support parametric routine spec, mark parameters as Const in typeinf
+        ci, _ = YaoCompiler.code_yao(YaoCompiler.Semantic.main, spec; optimize=true, passes=[:julia])
+        return YaoCompiler.codegen(target, ci)
+    end
 
-#         response = IBMQClient.create_remote_job(project, devices[3], access_token)
-#     end
-# end
+    qobj = IBMQClient.create_qobj(r.device, experiments...;
+        memory_slots=r.memory_slots, shots=r.nshots,
+            n_qubits=r.nqubits, r.options...)
 
+    return qobj
+end
 
-# params = rand(10)
-# @ibmq nshots=1024 begin
-#     for (α,β,γ) in params
-#         circuit(α, β, γ)
-#     end
-# end
+function submit(r::IBMQReg, specs::RoutineSpec...)
+    qobj = create_main_qobj(r, specs...)
+    IBMQClient.submit(r.account.project, r.device, qobj, r.account.access_token)
+end
+
+function YaoCompiler.execute(::typeof(YaoCompiler.Semantic.main), ::IBMQReg, ::RoutineSpec)
+    error("IBM Q devices is not fully compatible with YaoLang, please use @ibmq to define and submit jobs with a subset of YaoLang")
+end
+
+macro ibmq(ex...)
+    return ibmq_m(ex...)
+end
+
+function ibmq_m(ex...)
+    options = ex[1:end-1]
+    kwargs = []
+    for opt in options
+        opt isa Expr && opt.head === :(=) || error("invalid syntax $opt")
+        opt.args[1] in [:nshots, :max_credits, :seed, :schema_version] || error("invalid option: $opt")
+        push!(kwargs, Expr(:kw, opt.args[1], opt.args[2]))
+    end
+
+    experiment = last(ex)
+    experiment isa Expr || error("invalid syntax: $experiment")
+
+    seq = gensym(:seq)
+    return quote
+        $seq = Any[]
+        $(esc(collect_experiments(seq, experiment)))
+        r = $IBMQReg(;$(kwargs...))
+        $submit(r, $seq...)
+        r
+    end
+end
+
+function collect_experiments(seq::Symbol, @nospecialize(ex))
+    ex isa Expr || return ex # skip other things
+    ex.head == :call && return :(push!($seq, $ex))
+
+    if ex.head === :block
+        return Expr(:block, map(x->collect_experiments(seq, x), ex.args)...)
+    elseif ex.head === :for
+        return Expr(:for, ex.args[1], collect_experiments(seq, ex.args[2]))
+    else
+        error("invalid syntax: $ex")
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,34 @@ using Test
 
 using YaoLang.IBMQ
 
+@macroexpand IBMQ.@ibmq circuit()
+
+@macroexpand IBMQ.@ibmq begin
+    circuit1()
+    circuit2()
+end
+
+@macroexpand IBMQ.@ibmq begin
+    for (a, b) in zip(A, B)
+        circuit(a, b)
+    end
+end
+
 token = "d7339130578f8cc442dfcf260bee4049dfa25c6aabfd5ab771a693ec5cad1895f61f74f8aab7035c8ffddb83948ca17acef123c9955d9a554e02592eea5f6238"
 reg = IBMQ.IBMQReg(;token)
+using IBMQClient
+
+account = first(values(IBMQ.account_cache))
+access_token = first(values(IBMQ.account_cache)).access_token
+user_info = IBMQClient.user_info(auth, access_token)
+
+using REPL.TerminalMenus
+
+devices = IBMQClient.devices(account.project, account.access_token)
+m = IBMQ.DeviceMenu(devices, pagesize=4)
+devices[1]|>dump
+request(m)
+
 
 @testset "runtime" begin
     include("runtime/locations.jl")
