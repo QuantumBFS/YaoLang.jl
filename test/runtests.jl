2dcd3fd253f141b3504663d61a2a6236ac7fe834
Roger-luo <rogerluo.rl18@gmail.com>
2020-11-20 12:30:08 -0500

    polish test

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,14 +13,7 @@ using YaoCompiler.Intrinsics
     return c
 end
 
-@code_yao optimize=true circuit1()
-
-token = "e773394070269e3deace4372ed915c99610ee5a0e3be7b2f821e6889f4f4fe93cafdebcce46009e0ec9dd3ff8dca3ad3eb126b3bc59617ee837f2e120e99f268"
-reg = IBMQ.IBMQReg()
-job = IBMQ.submit(reg, test())
-result = IBMQClient.result(job)
-
-reg = IBMQ.IBMQReg(;nshots=2048)
+# token = "e773394070269e3deace4372ed915c99610ee5a0e3be7b2f821e6889f4f4fe93cafdebcce46009e0ec9dd3ff8dca3ad3eb126b3bc59617ee837f2e120e99f268"
 
 job = @ibmq nshots=2048 begin
     for i in 1:10
