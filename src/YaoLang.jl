8a7ed0ab679a2039477d97a578cffa3a26ae5ce6
Roger-Luo <rogerluo.rl18@gmail.com>
2021-05-06 00:25:01 -0400

    add deps

--- src/YaoLang.jl
+++ src/YaoLang.jl
@@ -4,9 +4,11 @@ using LinearAlgebra
 using YaoAPI
 using YaoCompiler
 using IBMQClient
+using YaoTargetQASM
+using YaoTargetQobj
 
 export @device, @qasm_str, @code_qasm, @code_yao, @ctrl, @measure, @gate
 
-include("ibmq.jl")
+
 
 end # module
